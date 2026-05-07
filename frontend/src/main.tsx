e59da65bb05cd0dfcfcb2dea5194307ac9d01216
Ashikur Rahman <ashikur.jebu111@gmail.com>
2026-05-07 20:50:21 +0600

    feat: add Vercel Analytics integration in frontend and update TypeScript configuration in backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,5 +23,6 @@ createRoot(document.getElementById("root")!).render(
         <ReactQueryDevtools initialIsOpen={false} />
       ) : null}
     </QueryClientProvider>
-  </StrictMode>,
+    <Analytics />
+  </StrictMode>
 );
